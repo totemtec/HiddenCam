7d9bcf3ec203964756b6c155b887cd4a82634257
马江林 <majianglin@totemtec.com>
2024-09-30 19:16:01 +0800

    simple

---
+++ sample/src/main/java/com/cottacush/android/hiddencam/oneshot/CameraFragment.kt
@@ -0,0 +1,324 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cottacush.android.hiddencam.oneshot
+
+import android.annotation.SuppressLint
+import android.content.*
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.hardware.display.DisplayManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.view.setPadding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.android.example.cameraxbasic.utils.MediaStoreUtils
+import com.cottacush.android.databinding.FragmentCameraBinding
+import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.launch
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+
+/**
+ * Main fragment for this app. Implements all camera operations including:
+ * - Viewfinder
+ * - Photo taking
+ * - Image analysis
+ */
+class CameraFragment : Fragment() {
+
+    private var _fragmentCameraBinding: FragmentCameraBinding? = null
+
+    private val fragmentCameraBinding get() = _fragmentCameraBinding!!
+
+    private lateinit var mediaStoreUtils: MediaStoreUtils
+
+    private var displayId: Int = -1
+    private var lensFacing: Int = CameraSelector.LENS_FACING_FRONT
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
+
+    /** Blocking camera operations are performed using this executor */
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroyView() {
+        _fragmentCameraBinding = null
+        super.onDestroyView()
+
+        // Shut down our background executor
+        cameraExecutor.shutdown()
+
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        _fragmentCameraBinding = FragmentCameraBinding.inflate(inflater, container, false)
+        return fragmentCameraBinding.root
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize our background executor
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        // Initialize MediaStoreUtils for fetching this app's images
+        mediaStoreUtils = MediaStoreUtils(requireContext())
+
+        // Wait for the views to be properly laid out
+        fragmentCameraBinding.viewFinder.post {
+
+            // Keep track of the display in which this view is attached
+            displayId = fragmentCameraBinding.viewFinder.display.displayId
+
+            // Build UI controls
+            updateCameraUi()
+
+            // Set up the camera and its use cases
+            lifecycleScope.launch {
+                setUpCamera()
+            }
+        }
+    }
+
+    /** Initialize CameraX, and prepare to bind the camera use cases  */
+    private suspend fun setUpCamera() {
+        cameraProvider = ProcessCameraProvider.getInstance(requireContext()).await()
+
+        // Build and bind the camera use cases
+        bindCameraUseCases()
+    }
+
+    /** Declare and bind preview, capture and analysis use cases */
+    private fun bindCameraUseCases() {
+
+        val bounds = requireActivity().windowManager.currentWindowMetrics.bounds
+
+        Log.d(TAG, "Screen metrics: ${bounds.width()} x ${bounds.height()}")
+
+        // CameraProvider
+        val cameraProvider = cameraProvider
+                ?: throw IllegalStateException("Camera initialization failed.")
+
+        // CameraSelector
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        // Preview
+        preview = Preview.Builder()
+                // We request aspect ratio but no resolution
+                //.setTargetAspectRatio(screenAspectRatio)
+                // Set initial target rotation
+//                .setTargetRotation(rotation)
+                .build()
+
+        // ImageCapture
+        imageCapture = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                // We request aspect ratio but no resolution to match preview config, but letting
+                // CameraX optimize for whatever specific resolution best fits our use cases
+//                .setTargetAspectRatio(screenAspectRatio)
+                // Set initial target rotation, we will have to call this again if rotation changes
+                // during the lifecycle of this use case
+//                .setTargetRotation(rotation)
+                .build()
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        if (camera != null) {
+            // Must remove observers from the previous camera instance
+            removeCameraStateObservers(camera!!.cameraInfo)
+        }
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            // Attach the viewfinder's surface provider to preview use case
+            preview?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+            observeCameraState(camera?.cameraInfo!!)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun removeCameraStateObservers(cameraInfo: CameraInfo) {
+        cameraInfo.cameraState.removeObservers(viewLifecycleOwner)
+    }
+
+    private fun observeCameraState(cameraInfo: CameraInfo) {
+        cameraInfo.cameraState.observe(viewLifecycleOwner) { cameraState ->
+            run {
+                when (cameraState.type) {
+                    CameraState.Type.PENDING_OPEN -> {
+                        // Ask the user to close other camera apps
+                        Log.d(TAG, "CameraState: Pending Open")
+                    }
+                    CameraState.Type.OPENING -> {
+                        // Show the Camera UI
+                        Log.d(TAG, "CameraState: Opening")
+                    }
+                    CameraState.Type.OPEN -> {
+                        // Setup Camera resources and begin processing
+                        Log.d(TAG, "CameraState: Open")
+                    }
+                    CameraState.Type.CLOSING -> {
+                        // Close camera UI
+                        Log.d(TAG, "CameraState: Closing")
+                    }
+                    CameraState.Type.CLOSED -> {
+                        // Free camera resources
+                        Log.d(TAG, "CameraState: Closed")
+                    }
+                }
+            }
+
+            cameraState.error?.let { error ->
+                when (error.code) {
+                    // Open errors
+                    CameraState.ERROR_STREAM_CONFIG -> {
+                        // Make sure to setup the use cases properly
+                        Toast.makeText(context,
+                                "Stream config error",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                    // Opening errors
+                    CameraState.ERROR_CAMERA_IN_USE -> {
+                        // Close the camera or ask user to close another camera app that's using the
+                        // camera
+                        Toast.makeText(context,
+                                "Camera in use",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                    CameraState.ERROR_MAX_CAMERAS_IN_USE -> {
+                        // Close another open camera in the app, or ask the user to close another
+                        // camera app that's using the camera
+                        Toast.makeText(context,
+                                "Max cameras in use",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                    CameraState.ERROR_OTHER_RECOVERABLE_ERROR -> {
+                        Toast.makeText(context,
+                                "Other recoverable error",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                    // Closing errors
+                    CameraState.ERROR_CAMERA_DISABLED -> {
+                        // Ask the user to enable the device's cameras
+                        Toast.makeText(context,
+                                "Camera disabled",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                    CameraState.ERROR_CAMERA_FATAL_ERROR -> {
+                        // Ask the user to reboot the device to restore camera function
+                        Toast.makeText(context,
+                                "Fatal error",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                    // Closed errors
+                    CameraState.ERROR_DO_NOT_DISTURB_MODE_ENABLED -> {
+                        // Ask the user to disable the "Do Not Disturb" mode, then reopen the camera
+                        Toast.makeText(context,
+                                "Do not disturb mode enabled",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+
+    /** Method used to re-draw the camera UI controls, called every time configuration changes. */
+    private fun updateCameraUi() {
+
+        // Listener for button used to capture photo
+        fragmentCameraBinding.cameraCaptureButton.setOnClickListener {
+
+            // Get a stable reference of the modifiable image capture use case
+            imageCapture?.let { imageCapture ->
+
+                // Create time stamped name and MediaStore entry.
+                val name = SimpleDateFormat(FILENAME, Locale.US)
+                    .format(System.currentTimeMillis())
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+                    put(MediaStore.MediaColumns.MIME_TYPE, PHOTO_TYPE)
+                    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                        val appName = requireContext().resources.getString(com.cottacush.android.hiddencam.R.string.app_name)
+                        put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/${appName}")
+                    }
+                }
+
+                // Create output options object which contains file + metadata
+                val outputOptions = ImageCapture.OutputFileOptions
+                    .Builder(requireContext().contentResolver,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues)
+                    .build()
+
+                // Setup image capture listener which is triggered after photo has been taken
+                imageCapture.takePicture(
+                        outputOptions, cameraExecutor, object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                    }
+
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        val savedUri = output.savedUri
+                        Log.d(TAG, "Photo capture succeeded: $savedUri")
+
+                    }
+                })
+            }
+        }
+    }
+
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val PHOTO_TYPE = "image/jpeg"
+    }
+}
